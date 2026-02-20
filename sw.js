3dfb2394de67f3081a4b9cfb4da26168c34de79e
pradolindalva4-sudo <pradolindalva4@gmail.com>
2026-02-20 15:23:38 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'jdp-industrial-v16.5';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('MOTOR: ARMAZENANDO ATIVOS');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
